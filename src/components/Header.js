43e1010416a23420a721d4d00bd3aef71608afc9
Niaz <mnuhaque@myseneca.ca>
2020-10-20 14:09:07 -0400

    Fixed inline, linting issues, Little colour change in UI : TBD bigger UI changes next push

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export const Header = () => {
-    return (
-        <h2>
-            Expense Tracker
-        </h2>
-    )
-}
+	return (
+		<h2>
+			<span id='header-1'>Expense Tracker</span>
+		</h2>
+	);
+};
